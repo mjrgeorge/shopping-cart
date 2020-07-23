c718debbb910bed8d03613915cafb537d0c97682
mjrgeorge <mjrgeorge@gmail.com>
2020-07-23 11:51:01 +0600

    some code organized

--- script.js
+++ script.js
@@ -1,90 +1,77 @@
 //IPHONE PLUS BUTTON HANDLER
 document.getElementById("iPhonePlusBtn").addEventListener("click", function () {
-    let iPhoneValue = document.getElementById("iPhoneValueBtn").value;
-    iPhoneValue = parseInt(iPhoneValue);
-    iPhoneValue = iPhoneValue + 1;
-    document.getElementById("iPhoneValueBtn").value = iPhoneValue;
+    quantityPlus("iPhoneValueBtn");
 
-    let iPhonePrice = document.getElementById("iPhonePriceBtn").innerText;
-    iPhonePrice = parseInt(iPhonePrice);
-    iPhonePrice = iPhonePrice + 1219;
-    document.getElementById("iPhonePriceBtn").innerText = iPhonePrice;
+    plus("iPhonePriceBtn", 1219);
 
-    let subTotal = document.getElementById("subTotalBtn").innerText;
-    subTotal = parseInt(subTotal);
-    subTotal = subTotal + 1219;
-    document.getElementById("subTotalBtn").innerText = subTotal;
+    plus("subTotalBtn", 1219);
 
-    let total = document.getElementById("totalBtn").innerText;
-    total = parseInt(total);
-    total = total + 1219;
-    document.getElementById("totalBtn").innerText = total;
+    plus("totalBtn", 1219);
 })
 
 //IPHONE MINUS BUTTON HANDLER
 document.getElementById("iPhoneMinusBtn").addEventListener("click", function () {
-    let iPhoneValue = document.getElementById("iPhoneValueBtn").value;
-    iPhoneValue = parseInt(iPhoneValue);
-    iPhoneValue = iPhoneValue - 1;
-    document.getElementById("iPhoneValueBtn").value = iPhoneValue;
+    quantityMinus("iPhoneValueBtn");
 
-    let iPhonePrice = document.getElementById("iPhonePriceBtn").innerText;
-    iPhonePrice = parseInt(iPhonePrice);
-    iPhonePrice = iPhonePrice - 1219;
-    document.getElementById("iPhonePriceBtn").innerText = iPhonePrice;
+    minus("iPhonePriceBtn", 1219);
 
-    let subTotal = document.getElementById("subTotalBtn").innerText;
-    subTotal = parseInt(subTotal);
-    subTotal = subTotal - 1219;
-    document.getElementById("subTotalBtn").innerText = subTotal;
+    minus("subTotalBtn", 1219);
 
-    let total = document.getElementById("totalBtn").innerText;
-    total = parseInt(total);
-    total = total - 1219;
-    document.getElementById("totalBtn").innerText = total;
+    minus("totalBtn", 1219);
 })
 
 //CASE PLUS BUTTON HANDLER
 document.getElementById("casePlusBtn").addEventListener("click", function () {
-    let caseValue = document.getElementById("caseValueBtn").value;
-    caseValue = parseInt(caseValue);
-    caseValue = caseValue + 1;
-    document.getElementById("caseValueBtn").value = caseValue;
 
-    let casePriceBtn = document.getElementById("casePriceBtn").innerText;
-    casePriceBtn = parseInt(casePriceBtn);
-    casePriceBtn = casePriceBtn + 59;
-    document.getElementById("casePriceBtn").innerText = casePriceBtn;
+    quantityPlus("caseValueBtn");
 
-    let subTotal = document.getElementById("subTotalBtn").innerText;
-    subTotal = parseInt(subTotal);
-    subTotal = subTotal + 59;
-    document.getElementById("subTotalBtn").innerText = subTotal;
+    plus("casePriceBtn", 59);
 
-    let total = document.getElementById("totalBtn").innerText;
-    total = parseInt(total);
-    total = total + 59;
-    document.getElementById("totalBtn").innerText = total;
+    plus("subTotalBtn", 59);
+
+    plus("totalBtn", 59);
 })
+
 //CASE MINUS BUTTON HANDLER
 document.getElementById("caseMinusBtn").addEventListener("click", function () {
-    let caseValue = document.getElementById("caseValueBtn").value;
-    caseValue = parseInt(caseValue);
-    caseValue = caseValue - 1;
-    document.getElementById("caseValueBtn").value = caseValue;
+    quantityMinus("caseValueBtn");
 
-    let casePriceBtn = document.getElementById("casePriceBtn").innerText;
-    casePriceBtn = parseInt(casePriceBtn);
-    casePriceBtn = casePriceBtn - 59;
-    document.getElementById("casePriceBtn").innerText = casePriceBtn;
+    minus("casePriceBtn", 59);
 
-    let subTotal = document.getElementById("subTotalBtn").innerText;
-    subTotal = parseInt(subTotal);
-    subTotal = subTotal - 59;
-    document.getElementById("subTotalBtn").innerText = subTotal;
+    minus("subTotalBtn", 59);
 
-    let total = document.getElementById("totalBtn").innerText;
-    total = parseInt(total);
-    total = total - 59;
-    document.getElementById("totalBtn").innerText = total;
+    minus("totalBtn", 59);
 })
+
+//FUNCTION
+function quantityPlus(id) {
+    let productQuantity = document.getElementById(id).value;
+    productQuantity = parseInt(productQuantity);
+    productQuantity = productQuantity + 1;
+    document.getElementById(id).value = productQuantity;
+    return productQuantity;
+}
+
+function quantityMinus(id) {
+    let productQuantity = document.getElementById(id).value;
+    productQuantity = parseInt(productQuantity);
+    productQuantity = productQuantity - 1;
+    document.getElementById(id).value = productQuantity;
+    return productQuantity;
+}
+
+function plus(id, num) {
+    let price = document.getElementById(id).innerText;
+    price = parseInt(price);
+    price = price + num;
+    document.getElementById(id).innerText = price;
+    return price;
+}
+
+function minus(id, num) {
+    let price = document.getElementById(id).innerText;
+    price = parseInt(price);
+    price = price - num;
+    document.getElementById(id).innerText = price;
+    return price;
+}
